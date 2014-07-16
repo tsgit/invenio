kaplun/ops

PdfChecker: skip records without unique ids in 037 tags

modules/docextract/lib/docextract_record.py
@@ -117,6 +117,13 @@ class BibRecord(object):
     def __getitem__(self, tag):
         return self.record[tag]
 
+    def get(self, tag, default):
+        try:
+            r = self[tag]
+        except IndexError:
+            r = default
+        return r
+
     def __eq__(self, b):
         if set(self.record.keys()) != set(b.record.keys()):
             return False

tsgit/invenio

modules/docextract/lib/docextract_record.py
@@ -156,6 +156,13 @@ class BibRecord(object):
             r = default
         return r
 
+    def get(self, tag, default):
+        try:
+            r = self[tag]
+        except KeyError:
+            r = default
+        return r
+
     def __eq__(self, b):
         if set(self.record.keys()) != set(b.record.keys()):
             return False